80be39ddc8e6af5433013e715e805ef32633045d
Il`ya_Black <tafaraylenovo@gmail.com>
2021-10-19 18:44:47 +0300

    класс виситор добавил

---
+++ Zoo-Project/Zoo-Project/Visitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo_Project
+{
+   public class Visitor
+    {
+            public int Age;
+            public double Money;
+
+            public Visitor(int age)
+            {
+                Age = age;
+
+                Random randomMoney = new Random();
+                Money = randomMoney.Next(1500, 2000);
+            }
+        }
+    }
+
+
