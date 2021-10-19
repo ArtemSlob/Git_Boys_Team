7f2d987d52d4a9bbc63743c91a1646b7ab47bb78
Kizliak <kizliakovich2@gmail.com>
2021-10-19 18:22:58 +0300

    Added Zoo.class

---
+++ Zoo-Project/Zoo-Project/Zoo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_09_28_Zoo
+{
+    static class Zoo
+    {
+        static public List<Animal> Animals = new List<Animal>()
+        {
+            new Animal("Tiger", 150, true),
+            new Animal("Wolf", 82, true),
+            new Animal("Chicken", 305, false),
+            new Animal("Phyton", 50, false),
+            new Animal("Bear", 220, false),
+            new Animal("Peacock", 180, false),
+            new Animal("Penguin", 120, false)
+        };
+
+        static public int VisitorAmount;
+        static public int AllVisitorAmount;
+        static public double Earnings;
+        static public double Treats = 100;
+        static public int MaxVisitors = 5;
+
+    }
+}
